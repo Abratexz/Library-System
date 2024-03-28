58b3c0441e8f59c585fe9b06d97d51cbb897af04
Abratexz <s6504062663141@email.kmutnb.ac.th>
2024-03-28 22:18:53 +0700

    UPDATE SETTING LIBRARY

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,36 @@
-var express = require('express');
-var router = express.Router();
+let express = require("express");
+let router = express.Router();
+let conn = require("./connect");
+let jwt = require("jsonwebtoken");
+let secretCode = "mylibraryprojectkey";
+let session = require("express-session");
+let formidable = require("formidable");
+let fs = require("fs");
+let dayjs = require("dayjs");
+let numeral = require("numeral");
+let dayFormat = "DD/MM/YYYY";
+router.use(
+  session({
+    secret: "sessionformylibraryprojectkey",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      MaxAge: 30 * 24 * 60 * 60 * 1000,
+    },
+  })
+);
+
+router.use((req, res, next) => {
+  res.locals.session = req.session;
+  res.locals.numeral = numeral;
+  res.locals.dayjs = dayjs;
+
+  next();
+});
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
 
 module.exports = router;
