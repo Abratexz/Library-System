9165d121e48d3100afc1e3761ddb85bf52a2163b
Abratexz <s6504062663141@email.kmutnb.ac.th>
2024-03-30 20:57:20 +0700

    My head is Explode
    borrow
    history
    and borrow history
    Page and Logic

--- app/routes/index.js
+++ app/routes/index.js
@@ -9,6 +9,8 @@ let fs = require("fs");
 let dayjs = require("dayjs");
 let numeral = require("numeral");
 let dayFormat = "DD/MM/YYYY HH:mm:ss";
+let path = require("path");
+
 router.use(
   session({
     secret: "sessionformylibraryprojectkey",
@@ -27,7 +29,18 @@ router.use((req, res, next) => {
 
   next();
 });
-
+const fetchGroupBooks = async (req, res, next) => {
+  try {
+    let conn = require("./connect2");
+    let sqlGroupBooks = "SELECT * FROM tb_group_book ORDER BY name_tag ASC";
+    let [groupBooksResult] = await conn.query(sqlGroupBooks);
+    req.groupBooks = groupBooksResult; // Attach groupBooks data to the request object
+    next(); // Call next to proceed to the next middleware/route handler
+  } catch (error) {
+    // Handle errors
+    res.status(500).send("Error fetching groupBooks data: " + error);
+  }
+};
 function isLogin(req, res, next) {
   if (req.session.token != undefined) {
     next();
@@ -50,31 +63,40 @@ router.get("/home", isLogin, async (req, res) => {
     let params = [];
     let sql = "SELECT COUNT(*) AS total FROM tb_book"; // Count total number of books
 
+    // Now fetch the books for the current page
+    let whereClause = "";
     if (req.query.search) {
-      sql += " WHERE name LIKE ?";
+      whereClause += "book_name LIKE(?)";
       params.push("%" + req.query.search + "%");
+    }
+    if (req.query.groupBookId) {
+      if (whereClause) whereClause += " AND ";
+      whereClause += "group_book_id = ?";
+      params.push(req.query.groupBookId);
+    }
+    if (whereClause) {
+      sql += " WHERE " + whereClause;
     }
 
     let [countResult] = await conn.query(sql, params);
     let totalBooks = countResult[0].total;
     let totalPages = Math.ceil(totalBooks / BooksPerPage);
-
-    // Now fetch the books for the current page
     sql = "SELECT * FROM tb_book";
-    if (req.query.search) {
-      sql += " WHERE name LIKE ?";
-      params.push("%" + req.query.search + "%");
+    if (whereClause) {
+      sql += " WHERE " + whereClause;
     }
     sql += " ORDER BY id DESC LIMIT ? OFFSET ?";
     params.push(BooksPerPage, offset);
 
     let [books, fields] = await conn.query(sql, params);
-
+    sql = "SELECT * FROM tb_group_book ORDER BY name_tag ASC";
+    let [groupBooks, fieldsGroupProduct] = await conn.query(sql);
     res.render("home", {
       books: books,
       currentPage: page,
       BooksPerPage: BooksPerPage,
       totalPages: totalPages,
+      groupBooks: groupBooks,
     });
   } catch (error) {
     res.send("Error: " + error);
@@ -135,73 +157,85 @@ router.post("/register", (req, res) => {
   });
 });
 
-router.get("/profile", isLogin, (req, res) => {
+router.get("/profile", isLogin, fetchGroupBooks, (req, res) => {
   let data = jwt.verify(req.session.token, secretCode);
   let sql = "SELECT * FROM tb_user WHERE id = ?";
   let params = [data.id];
   conn.query(sql, params, (err, result) => {
     if (err) throw err;
-    res.render("profile", { user: result[0] });
+    res.render("profile", { user: result[0], groupBooks: req.groupBooks });
   });
 });
 
-router.get("/editProfile/:id", isLogin, (req, res) => {
+router.get("/editProfile/:id", isLogin, fetchGroupBooks, (req, res) => {
   let sql = "SELECT * FROM tb_user WHERE id = ?";
   let params = req.params.id;
   conn.query(sql, params, (err, result) => {
     if (err) throw err;
-    res.render("editProfile", { user: result[0] });
+    res.render("editProfile", { user: result[0], groupBooks: req.groupBooks });
   });
 });
 
-router.post("/editProfile/:id", isLogin, (req, res) => {
+router.post("/editProfile/:id", isLogin, fetchGroupBooks, (req, res) => {
   let form = new formidable.IncomingForm();
   form.parse(req, (error, fields, file) => {
-    let filePath = file.img[0].filepath;
-    let newPath = "C://Users/nemo_/Desktop/Library-System/app/public/images/";
-    let pathUpload = newPath + file.img[0].originalFilename;
+    let sqlSelect = "SELECT img FROM tb_user WHERE id = ?";
+    let paramSelect = req.params.id;
+    conn.query(sqlSelect, paramSelect, (err, oldUser) => {
+      if (err) throw err;
+      let oldUserImg = oldUser[0];
+      let imgFileName = oldUserImg.img; // ค่าปกติของ Img ทื่มีอยู่ใน Data aka  รูปเก่า
+      if (file.img && file.img.length > 0) {
+        // ถ้ามีรูปใหม่อัปโหลดเข้ามา
+        let filePath = file.img[0].filepath;
+        let newPath =
+          "C://Users/nemo_/Desktop/Library-System/app/public/images/users/";
+        imgFileName = file.img[0].originalFilename; // ใช้รูปใหม่
 
-    fs.copyFile(filePath, pathUpload, () => {
-      let sqlSelect = "SELECT img FROM tb_user WHERE id = ?";
-      let paramSelect = req.params.id;
-
-      conn.query(sqlSelect, paramSelect, (err, oldImg) => {
-        if (err) throw err;
-        let newImg = oldImg[0];
-        fs.unlink(newPath + newImg.img, (err) => {
-          if (err) {
-            console.log(err);
+        fs.copyFile(filePath, newPath + imgFileName, () => {
+          if (err) throw err;
+          if (oldUserImg.img !== imgFileName) {
+            // ถ้ารูปใหม่ต่างจากรูปเก่า
+            fs.unlink(newPath + oldUserImg.img, (err) => {
+              if (err) {
+                console.log(err);
+              }
+            });
           }
-          // insert to database
-          let sql =
-            "UPDATE tb_user SET name = ? , usr = ?, pwd = ? , img = ? WHERE id = ?";
-          let params = [
-            fields["name"],
-            fields["usr"],
-            fields["pwd"],
-            file.img[0].originalFilename,
-            req.params.id,
-          ];
-          conn.query(sql, params, (err, result) => {
-            if (err) throw err;
-            res.redirect("/profile");
-          });
+        });
+      }
+      let sql =
+        "UPDATE tb_user SET name = ? , usr = ?, pwd = ? , img = ? WHERE id = ?";
+      let params = [
+        fields["name"],
+        fields["usr"],
+        fields["pwd"],
+        imgFileName,
+        req.params.id,
+      ];
+      conn.query(sql, params, (err, result) => {
+        if (err) throw err;
+
+        let sqlFetchUser = "SELECT * FROM tb_user WHERE id = ?";
+        conn.query(sqlFetchUser, paramSelect, (err, updatedUser) => {
+          req.session.img = updatedUser[0].img;
+          res.redirect("/profile");
         });
       });
     });
   });
 });
 
-router.get("/user", isLogin, (req, res) => {
+router.get("/user", isLogin, fetchGroupBooks, (req, res) => {
   let sql = "SELECT * FROM tb_user ORDER BY id DESC";
   conn.query(sql, (err, result) => {
     if (err) throw err;
-    res.render("user", { users: result });
+    res.render("user", { users: result, groupBooks: req.groupBooks });
   });
 });
 
-router.get("/addUser", isLogin, (req, res) => {
-  res.render("addUser", { user: {} });
+router.get("/addUser", isLogin, fetchGroupBooks, (req, res) => {
+  res.render("addUser", { user: {}, groupBooks: req.groupBooks });
 });
 
 router.post("/addUser", isLogin, (req, res) => {
@@ -213,12 +247,12 @@ router.post("/addUser", isLogin, (req, res) => {
   });
 });
 
-router.get("/editUser/:id", isLogin, (req, res) => {
+router.get("/editUser/:id", isLogin, fetchGroupBooks, (req, res) => {
   let sql = "SELECT * FROM tb_user WHERE id = ?";
   let params = req.params.id;
   conn.query(sql, params, (err, result) => {
     if (err) throw err;
-    res.render("addUser", { user: result[0] });
+    res.render("addUser", { user: result[0], groupBooks: req.groupBooks });
   });
 });
 
@@ -254,12 +288,14 @@ router.get("/groupBook", isLogin, (req, res) => {
   let sql = "SELECT * FROM tb_group_book ORDER BY id DESC";
   conn.query(sql, (err, result) => {
     if (err) throw err;
-    res.render("groupBook", { groupBooks: result });
+    res.render("groupBook", {
+      groupBooks: result,
+    });
   });
 });
 
-router.get("/addGroupBook", isLogin, (req, res) => {
-  res.render("addGroupBook", { groupBook: {} });
+router.get("/addGroupBook", isLogin, fetchGroupBooks, (req, res) => {
+  res.render("addGroupBook", { groupBook: {}, groupBooks: req.groupBooks });
 });
 
 router.post("/addGroupBook", isLogin, (req, res) => {
@@ -272,13 +308,16 @@ router.post("/addGroupBook", isLogin, (req, res) => {
   });
 });
 
-router.get("/editGroupBook/:id", isLogin, (req, res) => {
+router.get("/editGroupBook/:id", isLogin, fetchGroupBooks, (req, res) => {
   let sql = "SELECT * FROM tb_group_book WHERE id = ?";
   let params = req.params.id;
 
   conn.query(sql, params, (err, result) => {
     if (err) throw err;
-    res.render("addGroupBook", { groupBook: result[0] });
+    res.render("addGroupBook", {
+      groupBook: result[0],
+      groupBooks: req.groupBooks,
+    });
   });
 });
 
@@ -302,22 +341,26 @@ router.get("/deleteGroupBook/:id", isLogin, (req, res) => {
   });
 });
 
-router.get("/book", isLogin, (req, res) => {
+router.get("/book", isLogin, fetchGroupBooks, (req, res) => {
   let sql =
     "SELECT tb_book.* , tb_group_book.name_tag AS group_book_name_tag FROM tb_book " +
     "LEFT JOIN tb_group_book ON tb_group_book.id = tb_book.group_book_id " +
     "ORDER BY tb_book.id DESC";
   conn.query(sql, (err, result) => {
     if (err) throw err;
-    res.render("book", { books: result });
+    res.render("book", { books: result, groupBooks: req.groupBooks });
   });
 });
 
-router.get("/addBook", isLogin, (req, res) => {
+router.get("/addBook", isLogin, fetchGroupBooks, (req, res) => {
   let sql = "SELECT * FROM tb_group_book ORDER BY name_tag";
   conn.query(sql, (err, result) => {
     if (err) throw err;
-    res.render("addBook", { book: {}, groupBooks: result });
+    res.render("addBook", {
+      book: {},
+      groupBooks: result,
+      groupBooks: req.groupBooks,
+    });
   });
 });
 
@@ -329,7 +372,8 @@ router.post("/addBook", isLogin, (req, res) => {
       res.redirect("/book");
     } else {
       let filePath = file.img[0].filepath;
-      let newPath = "C://Users/nemo_/Desktop/Library-System/app/public/images/";
+      let newPath =
+        "C://Users/nemo_/Desktop/Library-System/app/public/images/books/";
       newPath += file.img[0].originalFilename;
       fs.copyFile(filePath, newPath, () => {
         let sql =
@@ -381,7 +425,7 @@ router.post("/editBook/:id", isLogin, (req, res) => {
         // ถ้ามีรูปใหม่อัปโหลดเข้ามา
         let filePath = file.img[0].filepath;
         let newPath =
-          "C://Users/nemo_/Desktop/Library-System/app/public/images/";
+          "C://Users/nemo_/Desktop/Library-System/app/public/images/books/";
         imgFileName = file.img[0].originalFilename; // ใช้รูปใหม่
 
         fs.copyFile(filePath, newPath + imgFileName, () => {
@@ -417,7 +461,8 @@ router.post("/editBook/:id", isLogin, (req, res) => {
 });
 
 router.get("/deleteBook/:id/:img", isLogin, (req, res) => {
-  let newPath = "C://Users/nemo_/Desktop/Library-System/app/public/images/";
+  let newPath =
+    "C://Users/nemo_/Desktop/Library-System/app/public/images/books";
 
   newPath += req.params.img;
 
@@ -434,7 +479,43 @@ router.get("/deleteBook/:id/:img", isLogin, (req, res) => {
   });
 });
 
-router.get("/borrow/:id", isLogin, (req, res) => {
+router.get("/deleteAllBook", isLogin, (req, res) => {
+  let newPath =
+    "C://Users/nemo_/Desktop/Library-System/app/public/images/books";
+
+  fs.readdir(newPath, (err, files) => {
+    if (err) throw err;
+
+    for (const file of files) {
+      const filePath = path.join(newPath, file);
+      fs.stat(filePath, (err, stats) => {
+        if (err) throw err;
+
+        if (stats.isFile()) {
+          fs.unlink(filePath, (err) => {
+            if (err) throw err;
+          });
+        }
+      });
+    }
+  });
+  let sql = "DELETE FROM tb_book";
+
+  conn.query(sql, (err, results) => {
+    if (err) throw err;
+    res.redirect("/book");
+  });
+});
+
+router.get("/setAllBookAvailable", isLogin, (req, res) => {
+  let sql = "UPDATE tb_book SET status = 'Available'";
+  conn.query(sql, (err, results) => {
+    if (err) throw err;
+    res.redirect("/book");
+  });
+});
+
+router.get("/borrow/:id", isLogin, fetchGroupBooks, (req, res) => {
   let sql = "SELECT * FROM tb_book WHERE id = ?";
   let params = req.params.id;
 
@@ -460,7 +541,7 @@ router.post("/borrow/:id", isLogin, async (req, res) => {
   let [bookStatusRow] = await conn.query(bookStatusQuery, [bookId]);
   let bookStatus = bookStatusRow[0].status;
 
-  if (bookStatus === "Borrowed") {
+  if (bookStatus === "Borrowed" || bookStatus === "Lost") {
     req.session.message = "FAILED TO BORROW";
     res.redirect("/home");
   } else {
@@ -500,7 +581,7 @@ router.post("/borrow/:id", isLogin, async (req, res) => {
   }
 });
 
-router.get("/history", isLogin, async (req, res) => {
+router.get("/history", isLogin, fetchGroupBooks, async (req, res) => {
   let conn = require("./connect2");
   let data = jwt.verify(req.session.token, secretCode);
 
@@ -516,7 +597,7 @@ router.get("/history", isLogin, async (req, res) => {
 
   try {
     let [results] = await conn.query(sql, params);
-    res.render("history", { books: results });
+    res.render("history", { books: results, groupBooks: req.groupBooks });
     console.log(results);
   } catch (err) {
     console.error(err);
@@ -524,22 +605,64 @@ router.get("/history", isLogin, async (req, res) => {
   }
 });
 
-router.get("/deleteHistory", isLogin, (req, res) => {
+router.get("/deleteHistory", isLogin, fetchGroupBooks, (req, res) => {
   let deleteHistorySql = "DELETE FROM tb_history WHERE user_id = ?";
-  let deleteBorrowSql = "DELETE FROM tb_borrow WHERE user_id = ?";
   let data = jwt.verify(req.session.token, secretCode);
   let params = [data.id];
   conn.query(deleteHistorySql, params, (err, result) => {
     if (err) throw err;
-    conn.query(deleteBorrowSql, params, (err, result) => {
+    let fetchSql = "SELECT * FROM tb_history WHERE user_id = ?";
+    conn.query(fetchSql, params, (err, history) => {
       if (err) throw err;
-      // Fetch the updated history (empty after deletion)
-      let fetchSql = "SELECT * FROM tb_history WHERE user_id = ?";
-      conn.query(fetchSql, params, (err, history) => {
-        if (err) throw err;
-        res.render("history", { books: history });
-      });
+      res.redirect("/history");
     });
+  });
+});
+
+router.get("/deleteHistory/:id", isLogin, fetchGroupBooks, (req, res) => {
+  let sql = "DELETE FROM tb_history WHERE user_id = ? AND book_id = ?";
+  let data = jwt.verify(req.session.token, secretCode);
+  let params = [data.id, req.params.id];
+  console.log(params);
+
+  conn.query(sql, params, (err, result) => {
+    if (err) throw err;
+    res.redirect("/history");
+  });
+});
+
+router.get("/borrowHistory", isLogin, fetchGroupBooks, (req, res) => {
+  let sql =
+    "SELECT tb_book.*, tb_borrow.id, tb_borrow.borrow_date, tb_borrow.return_date, tb_user.usr, tb_user.phone, tb_user.id AS tb_user_account_id " +
+    "FROM tb_borrow " +
+    "JOIN tb_book ON tb_borrow.book_id = tb_book.id " +
+    "JOIN tb_user ON tb_borrow.user_id = tb_user.id " +
+    "ORDER BY tb_borrow.id DESC";
+
+  conn.query(sql, (err, result) => {
+    if (err) throw err;
+    console.log(result);
+    res.render("borrowHistory", {
+      borrowHistory: result,
+      groupBooks: req.groupBooks,
+    });
+  });
+});
+
+router.get("/deleteborrowHistory", isLogin, (req, res) => {
+  let sql = "DELETE FROM tb_borrow";
+  conn.query(sql, (err, result) => {
+    if (err) throw err;
+    res.redirect("/borrowHistory");
+  });
+});
+
+router.get("/deleteborrowHistory/:id", isLogin, fetchGroupBooks, (req, res) => {
+  let sql = "DELETE FROM tb_borrow WHERE id = ?";
+  let params = req.params.id;
+  conn.query(sql, params, (err, result) => {
+    if (err) throw err;
+    res.redirect("/borrowHistory");
   });
 });
 module.exports = router;
