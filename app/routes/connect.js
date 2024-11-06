a66c75ac1936436733832f1601d69659aa320217
Abratexz <s6504062663141@email.kmutnb.ac.th>
2024-11-06 14:27:09 +0700

    TESTING SQL FROM ALWAYSDATA

--- app/routes/connect.js
+++ app/routes/connect.js
@@ -1,9 +1,16 @@
 let mysql = require("mysql");
 let conn = mysql.createConnection({
+  host: "mysql-library-system-kmutnb.alwaysdata.net",
+  user: "384028",
+  password: "library@1234",
+  database: "library-system-kmutnb_1",
+
+  /*
   host: "localhost",
   user: "root",
   password: "",
   database: "db_library",
+  */
 });
 
 conn.connect((err) => {
--- app/routes/connect2.js
+++ app/routes/connect2.js
@@ -1,10 +1,17 @@
 const mysql = require("mysql2");
 
 const pool = mysql.createPool({
+  host: "mysql-library-system-kmutnb.alwaysdata.net",
+  user: "384028",
+  password: "library@1234",
+  database: "library-system-kmutnb_1",
+  /*
   host: "localhost",
   user: "root",
   password: "",
   database: "db_library",
+  */
 });
+
 
 module.exports = pool.promise();
